49b5e8c67f203850c5745f0ceef53c72547aa4b5
Tim <timnew.wti@gmail.com>
2021-11-09 01:00:28 +1100

    ignore warning

--- lib/app/navigation/app_route_info_parser.dart
+++ lib/app/navigation/app_route_info_parser.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:router_example/app/navigation/navigation_item.dart';
